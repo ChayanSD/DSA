42be9f0e6fcde9acd93d5611c19a29c46c3921e8
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-07-05 20:23:12 +0600

    Binary search

--- src/Algorithms/Searching/BinarySearch/BinarySearch.java
+++ src/Algorithms/Searching/BinarySearch/BinarySearch.java
@@ -1,2 +1,24 @@
-package Algorithms.Searching.BinarySearch;public class BinarySearch {
+package Algorithms.Searching.BinarySearch;
+public class BinarySearch {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,6,7};
+        int target = 2;
+        System.out.println(binarySearch(arr,target));
+    }
+    static int binarySearch(int [] arr , int target){
+        int start = 0 ;
+        int end = arr.length-1;
+        while (start <= end){
+            //finding the mid
+            int mid = start + (end-start)/2;
+            if (target > arr[mid]){
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid -1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
