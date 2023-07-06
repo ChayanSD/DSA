fc57b3dfc0c248327f14492b7166a760ede99477
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-07-06 18:43:16 +0600

    Bubble sort code

--- src/Algorithms/Sorting/BubbleSort.java
+++ src/Algorithms/Sorting/BubbleSort.java
@@ -1,7 +1,24 @@
 package Algorithms.Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-
+        int [] arr = {4,3,2,1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            //for each step ,max item come at last respective index.
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j] < arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
     }
 }
