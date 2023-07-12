330bf52505427c37084d9f71e852c2a728199377
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-07-12 13:52:04 +0600

    works on get method

--- src/HashMap/MyMap.java
+++ src/HashMap/MyMap.java
@@ -13,17 +13,31 @@ public class MyMap<K,V> {
     public MyMap() {
         bucket = new ArrayList<>();
         capacity = INITIAL_CAPACITY;
-        for(int i = 0 ; i< capacity;i++){
+        for(int i = 0 ; i< capacity;i++)
             bucket.add(null);//This is a list that's reasons we initially put null at every index.
-        }
-
-
-
     }
+    private int getBucketIndex(K key){
+        int hashCode = key.hashCode();
+        return hashCode % capacity;
+    }
+    private V get(K key){
+        int bucketIndex = getBucketIndex(key);
+        MapNode<K,V> head = bucket.get(bucketIndex);
+        while (head != null){
+        if(head.key.equals(key)){
+            return head.value;
+        }
+        head = head.next;
+        }
+        return null;
+    }
+
+
 
     private class MapNode<K,V>{
         K key;
         V value;
+        MapNode<K , V> next;
 
         public MapNode(K key, V value) {
             this.key = key;
