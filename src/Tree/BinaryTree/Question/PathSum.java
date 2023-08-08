bf4c67ed4ca26bcbce91caa82e8f6efd443e4b0f
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-08-08 22:28:23 +0600

    Leetcode 112

---
+++ src/Tree/BinaryTree/Question/PathSum.java
@@ -0,0 +1,21 @@
+package Tree.BinaryTree.Question;
+
+public class PathSum {
+/*
+    public static boolean hasPathSum(PathSum root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        if(root.left == null && root.right==null){
+            return (targetSum-root.val)==0;
+        }
+
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+
+    }
+*/
+//Leetcode :https://leetcode.com/problems/path-sum/
+}
+
+
+
