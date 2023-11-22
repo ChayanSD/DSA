4cadec6923a3d1ad1c1ec7818349bcf38370267a
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-11-22 12:24:45 +0600

    LeetCode 107

---
+++ src/Tree/BinaryTree/Question/LevelOrder2.java
@@ -0,0 +1,51 @@
+package Tree.BinaryTree.Question;
+
+import java.util.*;
+
+public class LevelOrder2 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> result = levelOrderBottom(root);
+
+        System.out.print(Arrays.toString(result.toArray()));
+
+    }
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(0, currentLevel);
+        }
+
+
+        return result;
+
+    }
+
+}
+
+//LeetCode : 107 : https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
